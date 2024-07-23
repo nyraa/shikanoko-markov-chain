b83d69b76d0412511cad47276a26b78a350bba21
nyraa <112930946+nyraa@users.noreply.github.com>
2024-07-24 06:53:13 +0800

    Updates

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Shikanoko but it's a Markov chain",
+  description: 'A Markov chain to play the "Shikanoko"',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
