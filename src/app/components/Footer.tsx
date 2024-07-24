27822e7a7e92d6ad0ede0adbd9d92ce5acddc357
nyraa <112930946+nyraa@users.noreply.github.com>
2024-07-24 08:16:29 +0800

    add wiki link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,8 @@ export default function Footer()
 {
     return (
         <footer>
-            <a href="https://github.com/nyraa/shikanoko-markov-chain" target="_blank">GitHub</a><br />Inspired by <a href="https://youtu.be/Xkq13ZthmA0" target="_blank">【マルコフ連鎖】Shikanoko but it's a Markov chain</a>
+            <a href="https://github.com/nyraa/shikanoko-markov-chain" target="_blank">GitHub</a> - <a href="https://en.wikipedia.org/wiki/Markov_chain" target="_blank">What is Markov chain</a><br />
+            Inspired by <a href="https://youtu.be/Xkq13ZthmA0" target="_blank">【マルコフ連鎖】Shikanoko but it's a Markov chain</a>
         </footer>
     );
 }
