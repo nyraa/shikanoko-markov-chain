576352509446eb9ca0c9e7cb2297b070e9c4488c
nyraa <112930946+nyraa@users.noreply.github.com>
2024-07-24 12:04:23 +0800

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import "@app/styles/globals.css";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
     title: "Shikanoko but it's a Markov chain",
     description: 'A Markov chain to play the "Shikanoko"',
+    openGraph: {
+        images: [
+            {
+                url: "https://nyraa.github.io/shikanoko-markov-chain/thumbnail.png",
+                width: 1000,
+                height: 500,
+            }
+        ]
+    }
 }
 
 export default function RootLayout({
